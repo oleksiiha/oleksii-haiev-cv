d3bd1bec0cd8c55a3da18c814fd4c4fc6b4faaa0
Oleksii Haiev <oleksii@mycujoo.tv>
2024-01-17 14:16:44 +0100

    fixing build

--- next.config.js
+++ next.config.js
@@ -10,13 +10,13 @@ const withMDX = require('@next/mdx')({
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
-  output: 'export'
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  output: 'export'
 }
 
 // Merge MDX config with Next.js config
